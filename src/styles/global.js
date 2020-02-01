df5893e5d64320fccde8ddaa487b991925f5ee3b
Gustavo Oliveira <gmacedo@mcontigo.com>
2020-02-01 02:06:11 -0300

    ajustes

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,13 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
    * {
-        overflow: hidden;
         margin: 0;
         padding: 0;
         outline: 0;
         box-sizing: border-box;
    }
    html, body, #root {
+       overflow: hidden;
        width: 100%;
        height: 100vh;
        background-color: #E5E5E5;
